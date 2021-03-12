52cb78bb0fe272a6cd15a4af64be7298afbf63e2
Ziheng Chen <47013783+zihengc@users.noreply.github.com>
2021-03-12 11:27:58 -0500

    Delete Virus-Detection directory

--- Virus-Detection/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#Flag 1 is the number of cores
-core=$1
-
-#Download FastQC, RNAfold and index genome hg38
-snakemake --cores $core src/FastQC src/ORFfinder src/ViennaRNA-2.4.17/src/bin
-snakemake --cores $core src/hg38.fa
